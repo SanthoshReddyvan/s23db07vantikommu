2c0bded4a2f9d8f6d1777ffed46c5f25b43bf2d0
Santhosh Reddy Vantikommu <s560607@nwmissouri.edu>
2023-04-26 12:49:55 -0500

    bonus

--- models/rose.js
+++ models/rose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const roseSchema = mongoose.Schema({
-Rose_color: String,
-Rose_size: String,
-Rose_cost: String
+Rose_color: {type: String, minlength: 1, maxlength : 50},
+Rose_size: {type: String, minlength : 1, maxlength : 50},
+Rose_cost: {type : String, min : 1, maxlength : 30}
 })
 module.exports = mongoose.model("rose",roseSchema)
--- routes/rose.js
+++ routes/rose.js
@@ -2,19 +2,29 @@ var express = require('express');
 const rose_controlers= require('../controllers/rose');
 var router = express.Router();
 
+
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', rose_controlers.rose_view_all_Page );
 
 /* GET detail rose page */
-router.get('/detail', rose_controlers.rose_view_one_Page);
+router.get('/detail',secured, rose_controlers.rose_view_one_Page);
 
 /* GET create rose page */
-router.get('/create', rose_controlers.rose_create_Page);
+router.get('/create',secured, rose_controlers.rose_create_Page);
 
 /* GET create update page */
-router.get('/update', rose_controlers.rose_update_Page);
+router.get('/update',secured, rose_controlers.rose_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', rose_controlers.rose_delete_Page);
+router.get('/delete',secured, rose_controlers.rose_delete_Page);
 
 module.exports = router;
